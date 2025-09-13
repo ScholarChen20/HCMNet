daab314406ee348d3e97c698c7e0ffa1302462a2
ScholarChen20 <1523910137@qq.com>
2025-09-13 15:26:20 +0800

    BUS训练代码
    Signed-off-by: ScholarChen20 <1523910137@qq.com>

--- swin-umamba/train.py
+++ swin-umamba/train.py
@@ -122,7 +122,7 @@ def deep_main():
                                  avg_meters['val_pc'].avg, avg_meters['val_se'].avg, avg_meters['val_sp'].avg))
 
             accelerator.wait_for_everyone()
-            model_path = os.path.join("./output",config['model'],"Ablation_DDTI",config['model_pth']+"_HCMNet_150.pth")
+            model_path = os.path.join("./output",config['model'],"Ablation_DDTI",config['model_pth']+"_150_2.pth")
             if avg_meters['val_iou'].avg > best_iou:
                 best_iou = avg_meters['val_iou'].avg
                 unwrapped_model = accelerator.unwrap_model(model)
@@ -214,7 +214,7 @@ def Mamba_main():
                               % (avg_meters['val_iou'].avg, avg_meters['val_dice'].avg, avg_meters['val_acc'].avg,
                                  avg_meters['val_pc'].avg,avg_meters['val_se'].avg,avg_meters['val_sp'].avg))
             accelerator.wait_for_everyone()
-            model_path = os.path.join("./output",config['model'],config['model_pth']+"_150_1.pth")
+            model_path = os.path.join("./output",config['model'],config['model_pth']+"_150.pth")
             if avg_meters['val_iou'].avg > best_iou:
                 best_iou = avg_meters['val_iou'].avg
                 unwrapped_model = accelerator.unwrap_model(model)
@@ -306,7 +306,7 @@ def main():
         accelerator.print('val_iou %.4f - val_dice %.4f' % (avg_meters['val_iou'].avg, avg_meters['val_dice'].avg))
 
         accelerator.wait_for_everyone()
-        model_path = os.path.join("./output", config['model'], config['model_pth'] + "_150_2.pth")
+        model_path = os.path.join("./output", config['model'], config['model_pth'] + "_150.pth")
         if avg_meters['val_iou'].avg > best_iou:
             best_iou = avg_meters['val_iou'].avg
             unwrapped_model = accelerator.unwrap_model(model)

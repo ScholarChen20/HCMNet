34ac6d1e3bcf6d8c191ceff92d167330a06c2d79
ScholarChen20 <1523910137@qq.com>
2025-09-14 17:22:07 +0800

    BUS训练代码
    Signed-off-by: ScholarChen20 <1523910137@qq.com>

--- swin-umamba/nets/module/CDFormer.py
+++ swin-umamba/nets/module/CDFormer.py
@@ -315,49 +315,49 @@ class CDFormer(nn.Module):
         else:
             self.encoder, self.decoder = CDFormer_large(n_classes=n_classes)
 
-        for m in self.decoder.modules():
-            if isinstance(m, nn.Linear):
-                trunc_normal_(m.weight, std=.02)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.LayerNorm):
-                nn.init.constant_(m.bias, 0)
-                nn.init.constant_(m.weight, 1.0)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.bias, 0)
-                nn.init.constant_(m.weight, 1.0)
-            elif isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.ConvTranspose2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
+        # for m in self.decoder.modules():
+        #     if isinstance(m, nn.Linear):
+        #         trunc_normal_(m.weight, std=.02)
+        #         if m.bias is not None:
+        #             nn.init.constant_(m.bias, 0)
+        #     elif isinstance(m, nn.LayerNorm):
+        #         nn.init.constant_(m.bias, 0)
+        #         nn.init.constant_(m.weight, 1.0)
+        #     elif isinstance(m, nn.BatchNorm2d):
+        #         nn.init.constant_(m.bias, 0)
+        #         nn.init.constant_(m.weight, 1.0)
+        #     elif isinstance(m, nn.Conv2d):
+        #         nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        #         if m.bias is not None:
+        #             nn.init.constant_(m.bias, 0)
+        #     elif isinstance(m, nn.ConvTranspose2d):
+        #         nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        #         if m.bias is not None:
+        #             nn.init.constant_(m.bias, 0)
 
-        for selection in [self.encoder.cdca1,
-                          self.encoder.cdca2,
-                          self.encoder.cdca3,
-                          self.encoder.cdca4, ]:
-            for m in selection.modules():
-                if isinstance(m, nn.Linear):
-                    trunc_normal_(m.weight, std=.02)
-                    if m.bias is not None:
-                        nn.init.constant_(m.bias, 0)
-                elif isinstance(m, nn.LayerNorm):
-                    nn.init.constant_(m.bias, 0)
-                    nn.init.constant_(m.weight, 1.0)
-                elif isinstance(m, nn.BatchNorm2d):
-                    nn.init.constant_(m.bias, 0)
-                    nn.init.constant_(m.weight, 1.0)
-                elif isinstance(m, nn.Conv2d):
-                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                    if m.bias is not None:
-                        nn.init.constant_(m.bias, 0)
-                elif isinstance(m, nn.ConvTranspose2d):
-                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                    if m.bias is not None:
-                        nn.init.constant_(m.bias, 0)
+        # for selection in [self.encoder.cdca1,
+        #                   self.encoder.cdca2,
+        #                   self.encoder.cdca3,
+        #                   self.encoder.cdca4, ]:
+        #     for m in selection.modules():
+        #         if isinstance(m, nn.Linear):
+        #             trunc_normal_(m.weight, std=.02)
+        #             if m.bias is not None:
+        #                 nn.init.constant_(m.bias, 0)
+        #         elif isinstance(m, nn.LayerNorm):
+        #             nn.init.constant_(m.bias, 0)
+        #             nn.init.constant_(m.weight, 1.0)
+        #         elif isinstance(m, nn.BatchNorm2d):
+        #             nn.init.constant_(m.bias, 0)
+        #             nn.init.constant_(m.weight, 1.0)
+        #         elif isinstance(m, nn.Conv2d):
+        #             nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        #             if m.bias is not None:
+        #                 nn.init.constant_(m.bias, 0)
+        #         elif isinstance(m, nn.ConvTranspose2d):
+        #             nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        #             if m.bias is not None:
+        #                 nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         enc = self.encoder(x)

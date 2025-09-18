15c892f3bbed7a58ce79c0618163123acaf7cd94
ScholarChen20 <1523910137@qq.com>
2025-09-18 20:13:17 +0800

    BUSI->B测试
    Signed-off-by: ScholarChen20 <1523910137@qq.com>

--- swin-umamba/test.py
+++ swin-umamba/test.py
@@ -115,13 +115,13 @@ def Mamba_main():
     config = vars(parse_args())
     model = net(config['model'])
     #需要改
-    model_path = os.path.join("./output",config['model'],config['model_pth']+"_150_1.pth")
+    model_path = os.path.join("./output",config['model'],config['model_pth']+"_150.pth")
     model.load_state_dict(torch.load(model_path))
     model.eval()
     #需要改
     val_dataset = MedicineDataset(os.path.join(get_dataset(config["dataset"]), "test"), mode="val")  # 99
     # val_dataset = ThyroidDataset(os.path.join(get_dataset(config['dataset']),"test"), get_transform(train=False))
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size = 12, shuffle=False)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size = 8, shuffle=False)
     # val_dataset = MedicineDataset(os.path.join(get_dataset(config['dataset']), "test"), mode="val")
     # val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=16, shuffle=False)
     # val_dataset = PolypDataset(os.path.join(get_dataset(config['dataset']),"val-seg"),load_transform(train=False))
@@ -138,7 +138,7 @@ def Mamba_main():
     #需要改
     mask_pred = os.path.join("./output",config['model'],config['dataset'])
     #pred生成路径
-    file_dir = os.path.join(mask_pred, config['model']+'_pred_2_'+str(current_date.strftime("%Y-%m-%d")))
+    file_dir = os.path.join(mask_pred, config['model'] + '_pred_BUSI-B3_' + str(current_date.strftime("%Y-%m-%d")))
     os.makedirs(file_dir, exist_ok=True)
     file_path = file_dir + "/Metric.xlsx"
 
